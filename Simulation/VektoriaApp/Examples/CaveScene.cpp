0b1041abbedbe888fa156301856e0d50e4a1891d
VinzzF <funk.vin@t-online.de>
2022-04-07 20:34:17 +0200

    Improved Cave Scene sample with GeoWalls

--- Simulation/VektoriaApp/Examples/CaveScene.cpp
+++ Simulation/VektoriaApp/Examples/CaveScene.cpp
@@ -1,33 +1,35 @@
 #include "pch.h"
 #include "CaveScene.h"
 
-CaveScene::CaveScene()
+CaveScene::CaveScene(float fWidth /*= 8.0f*/, float fDepth /*= 6.0f*/, float fHeight /*= 5.0f*/, float fThickness /*= 0.1f*/)
+	: m_fWidth(fWidth)
+	, m_fDepth(fDepth)
+	, m_fHeight(fHeight)
+	, m_fThickness(fThickness)
 {
 	m_zpCamera.RotateX(UM_DEG2RAD(-20.0f));
-	m_zpCamera.TranslateDelta(0.0f, 12.0f, 35.0f);
+	m_zpCamera.TranslateDelta(fWidth * 0.5f, 12.0f, 25.0f);
 
 	// Boden
 	m_zmSides[0].LoadPreset("EarthCracked");
-	m_zgSides[0].Init(4.0f, 6.0f, &m_zmSides[0]);
+	m_zgSides[0].Init(fWidth, fDepth, fThickness, &m_zmSides[0]);
 	m_zpSides[0].RotateX(-HALFPI);
-	m_zpSides[0].TranslateYDelta(-5.0f);
 
 	// Seitenwand links
 	m_zmSides[1].LoadPreset("Rock");
-	m_zgSides[1].Init(6.0f, 5.0f, &m_zmSides[1]);
+	m_zgSides[1].Init(fDepth, fHeight, fThickness, &m_zmSides[1]);
 	m_zpSides[1].RotateY(HALFPI);
-	m_zpSides[1].TranslateXDelta(-4.0f);
 
 	// Seitenwand rechts
 	m_zmSides[2].LoadPreset("Rock");
-	m_zgSides[2].Init(6.0f, 5.0f, &m_zmSides[2]);
-	m_zpSides[2].RotateY(-HALFPI);
-	m_zpSides[2].TranslateXDelta(4.0f);
+	m_zgSides[2].Init(fDepth, fHeight, fThickness, &m_zmSides[2]);
+	m_zpSides[2].RotateY(HALFPI);
+	m_zpSides[2].TranslateXDelta(fWidth);
 
 	// Rückwand
 	m_zmSides[3].LoadPreset("CobblestoneMarble");
-	m_zgSides[3].Init(4.0f, 5.0f, &m_zmSides[3]);
-	m_zpSides[3].TranslateZ(-6.0f);
+	m_zgSides[3].Init(fWidth, fHeight, fThickness, &m_zmSides[3]);
+	m_zpSides[3].TranslateZ(-fDepth);
 
 	for (size_t i = 0u; i < 4u; ++i)
 	{
--- Simulation/VektoriaApp/Examples/CaveScene.h
+++ Simulation/VektoriaApp/Examples/CaveScene.h
@@ -8,11 +8,16 @@
 class CaveScene : public SimulationScene
 {
 public:
-	explicit CaveScene();
+	explicit CaveScene(float fWidth = 8.0f, float fDepth = 6.0f, float fHeight = 5.0f, float fThickness = 0.1f);
 	~CaveScene();
 
 protected:
+	const float m_fWidth;
+	const float m_fDepth;
+	const float m_fHeight;
+	const float m_fThickness;
+
 	Vektoria::CMaterial m_zmSides[4];
-	Vektoria::CGeoQuad m_zgSides[4];
+	Vektoria::CGeoWall m_zgSides[4];
 	Vektoria::CPlacement m_zpSides[4];
 };
