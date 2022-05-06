400ce4578389cb28ac331e53e69f270a23cb6fc7
VinzzF <funk.vin@t-online.de>
2022-05-06 10:30:39 +0200

    Added specifiable W-component to homogenous vector conversion

--- Simulation/VektoriaApp/PhysicInterfaces/Converter.h
+++ Simulation/VektoriaApp/PhysicInterfaces/Converter.h
@@ -3,9 +3,9 @@
 
 #include <glm/glm.hpp>
 
-static Vektoria::CHVector convertVector(const glm::vec3& vec)
+static Vektoria::CHVector convertVector(const glm::vec3& vec, float w = 1.0f)
 {
-	return Vektoria::CHVector(vec.x, vec.y, vec.z);
+	return Vektoria::CHVector(vec.x, vec.y, vec.z, w);
 }
 
 static glm::vec3 convertVector(const Vektoria::CHVector& vec)
